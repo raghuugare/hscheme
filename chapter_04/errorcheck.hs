bdc4b525b47abfeea0ceeb9342f498ba87b6fa82
Raghu Ugare <raghu.ugare@gmail.com>
2016-10-03 23:47:01 +0530

    Error-checking begun...

---
+++ chapter_04/errorcheck.hs
@@ -0,0 +1,128 @@
+module Main where
+import System.Environment
+import Text.ParserCombinators.Parsec hiding(spaces)
+import Control.Monad
+import Control.Monad.Error
+
+main:: IO ()
+main = do
+  args <- getArgs
+  evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
+  putStrLn $ extractValue $ trapError evaled
+  
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> ThrowsError LispVal
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> throwError $ Parser err
+  Right val -> return val
+                  
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+instance Show LispVal where
+  show = showVal
+
+showVal :: LispVal -> String
+showVal (Atom name)             = name
+showVal (List l)                = "(" ++ unwordsList l ++ ")"
+showVal (DottedList head tail)  = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+showVal (Number number)         = show number
+showVal (String stringVal)      = "\"" ++ stringVal ++ "\""
+showVal (Bool True)             = "#t"
+showVal (Bool False)            = "#f"
+
+eval :: LispVal -> ThrowsError LispVal
+eval val@(String _)             = return val
+eval val@(Number _)             = return val
+eval val@(Bool _)               = return val
+eval (List [Atom "quote", val]) = return val
+eval (List (Atom func: args))   = mapM eval args >>= apply func
+eval badForm                    = throwError $ BadSpecialForm "Unrecognized special form" badForm
+
+apply :: String -> [LispVal] -> ThrowsError LispVal
+apply func args = maybe (throwError $ NotFunction "Unrecognized primitive function args: " func )
+                  ($ args)
+                  (lookup func primitives)
+
+primitives :: [(String, [LispVal] -> ThrowsError LispVal)]
+primitives = [("+",         numericBinop (+)),
+              ("-",         numericBinop (-)),
+              ("*",         numericBinop (*)),
+              ("/",         numericBinop div),
+              ("mod",       numericBinop mod),
+              ("quotient",  numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
+numericBinop op singleVal@[_]  = throwError $ NumArgs 2 singleVal 
+numericBinop op params         = mapM unpackNum params >>= return . Number . fold1 op 
+
+unpackNum :: LispVal -> ThrowsError Integer
+unpackNum (Number n) = return n
+unpackNum (String n) = let parsed = reads n in
+                         if null parsed then throwError $ TypeMismatch $ String n
+                         else return $ fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum notNum = throwError $TypeMismatch "number" notNum
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+data LispError   = NumArgs Integer [LispVal]
+                 | TypeMismatch String LispVal
+                 | Parser ParseError
+                 | BadSpecialForm String LispVal
+                 | NotFunction String String
+                 | UnboundVar String String
+                 | Default String
+
+parseString :: Parser LispVal
+parseString = do char '"'
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest  <- many(letter <|> symbol <|> digit)
+               let atom = [first] ++ rest
+               return $ case atom of
+                 "#t" -> Bool True
+                 "#f" -> Bool False
+                 otherwise -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr =  parseAtom
+         <|> parseString
+         <|> parseNumber
+         <|> parseQuoted
+         <|> do char '('
+                x <- (try parseList) <|> parseDottedList
+                char ')'
+                return x
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do head <- endBy parseExpr spaces
+                     tail <- char '.' >> spaces >> parseExpr
+                     return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do char '\''
+                 x <- parseExpr
+                 return $ List [Atom "quote", x]
+
