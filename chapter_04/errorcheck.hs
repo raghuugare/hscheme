6e7e4f0d1bd1ff8df3cfb1f1a1aacdeb1ccf4697
Raghu Ugare <raghu.ugare@gmail.com>
2016-10-04 11:44:00 +0530

    Minor formatting.

--- chapter_04/errorcheck.hs
+++ chapter_04/errorcheck.hs
@@ -86,18 +86,18 @@ data LispError   = NumArgs Integer [LispVal]
                  | Default String
 
 showError :: LispError -> String
-showError (UnboundVar message varName) = message ++ ": " ++ varName
+showError (UnboundVar message varName)  = message ++ ": " ++ varName
 showError (BadSpecialForm message form) = message ++ ": " ++ show form
-showError (NotFunction message func) = message ++ ": " ++ show func
-showError (NumArgs expected found) = "Expected " ++ show expected  ++ " args! Found values " ++ unwordsList found
+showError (NotFunction message func)    = message ++ ": " ++ show func
+showError (NumArgs expected found)      = "Expected " ++ show expected  ++ " args! Found values " ++ unwordsList found
 showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected ++ " , found " ++ show found
-showError (Parser parseErr) = "Parse error at " ++ show parseErr
+showError (Parser parseErr)             = "Parse error at " ++ show parseErr
 
 instance Show LispError where show = showError
 
 instance Error LispError where
-noMsg = Default "An error has occurred "
-strMsg = Default
+  noMsg  = Default "An error has occurred "
+  strMsg = Default
 
 type ThrowsError = Either LispError
 
